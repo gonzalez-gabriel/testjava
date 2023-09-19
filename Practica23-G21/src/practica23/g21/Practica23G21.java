b52d095d07fcad6ce57784bd26822be30ea94451
Facundo <facuon1621@gmail.com>
2023-09-19 15:27:16 -0300

    Creacion de proyecto y clase Producto

---
+++ Practica23-G21/src/practica23/g21/Practica23G21.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica23.g21;
+
+/**
+ *
+ * @author estudiante
+ */
+public class Practica23G21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
