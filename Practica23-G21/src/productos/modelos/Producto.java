b3ed32b8d66a288a0f9bb8bfff7bd5789d24f365
Facundo <facuon1621@gmail.com>
2023-09-19 15:59:22 -0300

    Agregado de clases: Cliente, Empleado y Encargado Agregado de .gitignore

--- Practica23-G21/src/productos/modelos/Producto.java
+++ Practica23-G21/src/productos/modelos/Producto.java
@@ -9,5 +9,23 @@ package productos.modelos;
  * @author estudiante
  */
 public class Producto {
+    public String nombre;
+    public String descripcion;
+    public float precio;
+    public boolean estado;
+    public String tipo;
     
+    void mostrar() {
+        System.out.println("Producto: " + nombre);
+        System.out.println("Descripcion: " + descripcion);
+        System.out.println("Precio: " + precio);
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Estado: \n");
+        if(estado) {
+            System.out.println("Disponible");
+        }
+        else {
+            System.out.println("No disponible");
+        }
+    }
 }
