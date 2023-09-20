47e3382e76d9c5777b42f60b6771a715f08b85ba
Gabriel Gonzalez <gabriel.gonzalez.unt@gmail.com>
2023-09-19 23:31:25 -0300

    2

---
+++ src/newpackage2/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package newpackage2;
+
+/**
+ *
+ * @author gabri
+ */
+public class NewClass {
+    int number= 3;
+}
