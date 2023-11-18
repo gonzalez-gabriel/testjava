9c74bc330fdf20ccb91fd84003c2c9c29afe794a
Facundo Ordoñez Nadra <facuon1621@gmail.com>
2023-11-18 17:02:18 -0300

    Resuelto Tp5 punto 3 (18/11)

--- Practica23-G21/src/principal/controladores/ControladorPrincipal.java
+++ Practica23-G21/src/principal/controladores/ControladorPrincipal.java
@@ -5,11 +5,7 @@
  */
 package principal.controladores;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import usuarios.modelos.*;
-import pedidos.modelos.Pedido;
-import pedidos.modelos.ProductoDelPedido;
+import pedidos.modelos.*;
 import productos.modelos.*;
 
 
@@ -21,10 +17,10 @@ public class ControladorPrincipal  {
     public static void main(String[] args) {
         
         GestorProductos gp = GestorProductos.crear();
-        System.out.println(gp.crearProducto(1,"Papafrita" , 10, Categoria.ENTRADA, Estado.DISPONIBLE));
-        System.out.println(gp.crearProducto(2,"Milanga" , 20, Categoria.PLATOPRINCIPAL, Estado.DISPONIBLE));
-        System.out.println(gp.crearProducto(3,"Helado gridito" , 30, Categoria.POSTRE, Estado.DISPONIBLE));
-        System.out.println(gp.crearProducto(4,"Choriflan" , 40, Categoria.PLATOPRINCIPAL, Estado.DISPONIBLE));
-  
+        GestorPedidos gped = GestorPedidos.crear();
+        System.out.println(gp.crearProducto(1,"Papafrita" , 10, Categoria.ENTRADA, productos.modelos.Estado.DISPONIBLE));
+        System.out.println(gp.crearProducto(2,"Milanga" , 20, Categoria.PLATOPRINCIPAL, productos.modelos.Estado.DISPONIBLE));
+        System.out.println(gp.crearProducto(3,"Helado gridito" , 30, Categoria.POSTRE, productos.modelos.Estado.DISPONIBLE));
+        System.out.println(gp.crearProducto(4,"Choriflan" , 40, Categoria.PLATOPRINCIPAL, productos.modelos.Estado.DISPONIBLE));
     }
 }
