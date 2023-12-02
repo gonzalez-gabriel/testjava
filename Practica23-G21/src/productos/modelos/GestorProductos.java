e0d5c51319fd870b178d79e9a634f05f6d1a2683
Guido González <guiagugon@gmail.com>
2023-12-02 15:10:37 -0300

    12/2

--- Practica23-G21/src/productos/modelos/GestorProductos.java
+++ Practica23-G21/src/productos/modelos/GestorProductos.java
@@ -55,6 +55,7 @@ public class GestorProductos implements IGestorProductos {
             }
             else {               
                 this.productos.add(p);
+                this.guardarEnArchivo();
                 return EXITO_CREADO;
             }            
         }
